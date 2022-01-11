5ada25380fe7b0a84794d9e4a8c6dd0acd2d64c0
Yusen Hu <richardyusen@gmail.com>
2022-01-11 20:11:00 +0000

    Style cleanup for tx.c

--- tx.c
+++ tx.c
@@ -19,15 +19,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
 #include <time.h>
 
 #include "fec.h"
-
 #include "lib.h"
 #include "wifibroadcast.h"
-
-
 
 #define MAX_PACKET_LENGTH 4192
 #define MAX_USER_PACKET_LENGTH 1450
@@ -37,21 +33,20 @@
 #define MAX_FIFOS 8
 
 
-/* this is the template radiotap header we send packets out with */
+// This is the template radiotap header we send packets out with
 
 static const u8 u8aRadiotapHeader[] = {
-
-	0x00, 0x00, // <-- radiotap version
-	0x0c, 0x00, // <- radiotap header lengt
-	0x04, 0x80, 0x00, 0x00, // <-- bitmap
+	0x00, 0x00,	// radiotap version
+	0x0c, 0x00,	// radiotap header lengt
+	0x04, 0x80, 0x00, 0x00,	// bitmap
 	0x22, 
 	0x0, 
 	0x18, 0x00 
 };
 
-/* Penumbra IEEE80211 header */
+// Penumbra IEEE80211 header
 
-//the last byte of the mac address is recycled as a port number
+// The last byte of the mac address is recycled as a port number
 #define SRC_MAC_LASTBYTE 15
 #define DST_MAC_LASTBYTE 21
 
@@ -63,15 +58,9 @@ static u8 u8aIeeeHeader[] = {
 	0x10, 0x86,
 };
 
-
-
 int flagHelp = 0;
 
-
-
-void
-usage(void)
-{
+void usage(void) {
 	printf(
 	    "(c)2015 befinitiv. Based on packetspammer by Andy Green.  Licensed under GPL2\n"
 	    "\n"
@@ -94,11 +83,10 @@ usage(void)
 }
 
 void set_port_no(uint8_t *pu, uint8_t port) {
-	//dirty hack: the last byte of the mac address is the port number. this makes it easy to filter out specific ports via wireshark
+	// Dirty hack: the last byte of the mac address is the port number. this makes it easy to filter out specific ports via wireshark
 	pu[sizeof(u8aRadiotapHeader) + SRC_MAC_LASTBYTE] = port;
 	pu[sizeof(u8aRadiotapHeader) + DST_MAC_LASTBYTE] = port;
 }
-
 
 typedef struct {
 	int seq_nr;
@@ -107,45 +95,37 @@ typedef struct {
 	packet_buffer_t *pbl;
 } fifo_t;
 
-	
-
 int packet_header_init(uint8_t *packet_header) {
-			u8 *pu8 = packet_header;
-			memcpy(packet_header, u8aRadiotapHeader, sizeof(u8aRadiotapHeader));
-			pu8 += sizeof(u8aRadiotapHeader);
-			memcpy(pu8, u8aIeeeHeader, sizeof (u8aIeeeHeader));
-			pu8 += sizeof (u8aIeeeHeader);
-					
-			//determine the length of the header
-			return pu8 - packet_header;
+	u8 *pu8 = packet_header;
+	memcpy(packet_header, u8aRadiotapHeader, sizeof(u8aRadiotapHeader));
+	pu8 += sizeof(u8aRadiotapHeader);
+	memcpy(pu8, u8aIeeeHeader, sizeof (u8aIeeeHeader));
+	pu8 += sizeof (u8aIeeeHeader);
+			
+	// Determine the length of the header
+	return pu8 - packet_header;
 }
 
 void fifo_init(fifo_t *fifo, int fifo_count, int block_size) {
-	int i;
-
-	for(i=0; i<fifo_count; ++i) {
-		int j;
-
+	for(int i=0; i<fifo_count; i++) {
 		fifo[i].seq_nr = 0;
 		fifo[i].fd = -1;
 		fifo[i].curr_pb = 0;
 		fifo[i].pbl = lib_alloc_packet_buffer_list(block_size, MAX_PACKET_LENGTH);
 
-		//prepare the buffers with headers
-		for(j=0; j<block_size; ++j) {
+		// Prepare the buffers with headers
+		for(int j=0; j<block_size; ++j) {
 			fifo[i].pbl[j].len = 0;
 		}
 	}
-
 }
 
 void fifo_open(fifo_t *fifo, int fifo_count) {
-	int i;
 	if(fifo_count > 1) {
-		//new FIFO style
+		// New FIFO style
 		
-		//first, create all required fifos
-		for(i=0; i<fifo_count; ++i) {
+		// First, create all required fifos
+		for(int i=0; i<fifo_count; ++i) {
 			char fn[256];
 			sprintf(fn, FIFO_NAME, i);
 			
@@ -156,8 +136,8 @@ void fifo_open(fifo_t *fifo, int fifo_count) {
 			}
 		}
 		
-		//second: wait for the data sources to connect
-		for(i=0; i<fifo_count; ++i) {
+		// Second: wait for the data sources to connect
+		for(int i=0; i<fifo_count; ++i) {
 			char fn[256];
 			sprintf(fn, FIFO_NAME, i);
 			
@@ -170,18 +150,16 @@ void fifo_open(fifo_t *fifo, int fifo_count) {
 		}
 	}
 	else {
-		//old style STDIN input
+		// Old style STDIN input
 		fifo[0].fd = STDIN_FILENO;
 	}
 }
 
-
 void fifo_create_select_set(fifo_t *fifo, int fifo_count, fd_set *fifo_set, int *max_fifo_fd) {
-	int i;
 
 	FD_ZERO(fifo_set);
 	
-	for(i=0; i<fifo_count; ++i) {
+	for(int i=0; i<fifo_count; ++i) {
 		FD_SET(fifo[i].fd, fifo_set);
 
 		if(fifo[i].fd > *max_fifo_fd) {
@@ -190,9 +168,8 @@ void fifo_create_select_set(fifo_t *fifo, int fifo_count, fd_set *fifo_set, int 
 	}
 }
 
-
 void pb_transmit_packet(pcap_t *ppcap, int seq_nr, uint8_t *packet_transmit_buffer, int packet_header_len, const uint8_t *packet_data, int packet_length) {
-    //add header outside of FEC
+    // Add header outside of FEC
     wifi_packet_header_t *wph = (wifi_packet_header_t*)(packet_transmit_buffer + packet_header_len);
     wph->sequence_number = seq_nr;
 
@@ -207,26 +184,17 @@ void pb_transmit_packet(pcap_t *ppcap, int seq_nr, uint8_t *packet_transmit_buff
     }
 }
 
-
-
-
 void pb_transmit_block(packet_buffer_t *pbl, pcap_t *ppcap, int *seq_nr, int port, int packet_length, uint8_t *packet_transmit_buffer, int packet_header_len, int data_packets_per_block, int fec_packets_per_block, int transmission_count) {
-	int i;
 	uint8_t *data_blocks[MAX_DATA_OR_FEC_PACKETS_PER_BLOCK];
 	uint8_t fec_pool[MAX_DATA_OR_FEC_PACKETS_PER_BLOCK][MAX_USER_PACKET_LENGTH];
 	uint8_t *fec_blocks[MAX_DATA_OR_FEC_PACKETS_PER_BLOCK];
 
-
-	for(i=0; i<data_packets_per_block; ++i) {
+	for(int i=0; i<data_packets_per_block; i++)
 		data_blocks[i] = pbl[i].data;
-	}
-
 
 	if(fec_packets_per_block) {
-		for(i=0; i<fec_packets_per_block; ++i) {
+		for(int i=0; i<fec_packets_per_block; i++)
 			fec_blocks[i] = fec_pool[i];
-		}
-
 		fec_encode(packet_length, data_blocks, data_packets_per_block, (unsigned char **)fec_blocks, fec_packets_per_block);
 	}
 
@@ -234,10 +202,8 @@ void pb_transmit_block(packet_buffer_t *pbl, pcap_t *ppcap, int *seq_nr, int por
 	set_port_no(pb, port);
 	pb += packet_header_len;
 
-
-	int x;
-	for(x=0; x<transmission_count; ++x) {
-		//send data and FEC packets interleaved
+	for(int x=0; x<transmission_count; x++) {
+		// Send data and FEC packets interleaved
 		int di = 0; 
 		int fi = 0;
 		int seq_nr_tmp = *seq_nr;
@@ -258,21 +224,14 @@ void pb_transmit_block(packet_buffer_t *pbl, pcap_t *ppcap, int *seq_nr, int por
 
 	*seq_nr += data_packets_per_block + fec_packets_per_block;
 
-
-
-	//reset the length back
-	for(i=0; i< data_packets_per_block; ++i) {
-			pbl[i].len = 0;
-	}
-
+	// Reset the length back
+	for(int i=0; i< data_packets_per_block; i++)
+		pbl[i].len = 0;
 }
 
 
-int
-main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	char szErrbuf[PCAP_ERRBUF_SIZE];
-	int i;
 	pcap_t *ppcap = NULL;
 	char fBrokenSocket = 0;
 	int pcnt = 0;
@@ -283,15 +242,13 @@ main(int argc, char *argv[])
 	int max_fifo_fd = -1;
 	fifo_t fifo[MAX_FIFOS];
 
-		int param_transmission_count = 1;
+	int param_transmission_count = 1;
     int param_data_packets_per_block = 8;
     int param_fec_packets_per_block = 4;
 	int param_packet_length = MAX_USER_PACKET_LENGTH;
 	int param_port = 0;
 	int param_min_packet_length = 0;
 	int param_fifo_count = 1;
-
-
 
 	printf("Raw data transmitter (c) 2015 befinitiv  GPL2\n");
 
@@ -301,135 +258,125 @@ main(int argc, char *argv[])
 			{ "help", no_argument, &flagHelp, 1 },
 			{ 0, 0, 0, 0 }
 		};
-		int c = getopt_long(argc, argv, "r:hf:p:b:m:s:x:",
-			optiona, &nOptionIndex);
+		int c = getopt_long(argc, argv, "r:hf:p:b:m:s:x:", optiona, &nOptionIndex);
 
 		if (c == -1)
 			break;
+
 		switch (c) {
-		case 0: // long option
-			break;
+			case 0:		// Long option
+				break;
 
-		case 'h': // help
-			usage();
+			case 'h':	// Help
+				usage();
 
-		case 'r': // retransmissions
-			param_fec_packets_per_block = atoi(optarg);
-			break;
+			case 'r':	// Retransmissions
+				param_fec_packets_per_block = atoi(optarg);
+				break;
 
-		case 'f': // MTU
-			param_packet_length = atoi(optarg);
-			break;
+			case 'f':	// MTU
+				param_packet_length = atoi(optarg);
+				break;
 
-		case 'p': //port
-			param_port = atoi(optarg);
-			break;
+			case 'p':	// Port
+				param_port = atoi(optarg);
+				break;
 
-		case 'b': //retransmission block size
-			param_data_packets_per_block = atoi(optarg);
-			break;
+			case 'b':	// Retransmission block size
+				param_data_packets_per_block = atoi(optarg);
+				break;
 
-		case 'm'://minimum packet length
-			param_min_packet_length = atoi(optarg);
-			break;
+			case 'm':	// Minimum packet length
+				param_min_packet_length = atoi(optarg);
+				break;
 
-		case 's': //how many streams (fifos) do we have in parallel
-			param_fifo_count = atoi(optarg);
-			break;
+			case 's':	// How many streams (fifos) do we have in parallel
+				param_fifo_count = atoi(optarg);
+				break;
 
-		case 'x': //how often is a block transmitted
-			param_transmission_count = atoi(optarg);
-			break;
+			case 'x':	// How often is a block transmitted
+				param_transmission_count = atoi(optarg);
+				break;
 
-		default:
-			fprintf(stderr, "unknown switch %c\n", c);
-			usage();
-			break;
+			default:
+				fprintf(stderr, "unknown switch %c\n", c);
+				usage();
+				break;
 		}
 	}
 
 	if (optind >= argc)
 		usage();
-
 	
 	if(param_packet_length > MAX_USER_PACKET_LENGTH) {
 		fprintf(stderr, "Packet length is limited to %d bytes (you requested %d bytes)\n", MAX_USER_PACKET_LENGTH, param_packet_length);
-		return (1);
+		return 1;
 	}
 
 	if(param_min_packet_length > param_packet_length) {
 		fprintf(stderr, "Your minimum packet length is higher that your maximum packet length (%d > %d)\n", param_min_packet_length, param_packet_length);
-		return (1);
+		return 1;
 	}
 
 	if(param_fifo_count > MAX_FIFOS) {
 		fprintf(stderr, "The maximum number of streams (FIFOS) is %d (you requested %d)\n", MAX_FIFOS, param_fifo_count);
-		return (1);
+		return 1;
 	}
 
 	if(param_data_packets_per_block > MAX_DATA_OR_FEC_PACKETS_PER_BLOCK || param_fec_packets_per_block > MAX_DATA_OR_FEC_PACKETS_PER_BLOCK) {
 		fprintf(stderr, "Data and FEC packets per block are limited to %d (you requested %d data, %d FEC)\n", MAX_DATA_OR_FEC_PACKETS_PER_BLOCK, param_data_packets_per_block, param_fec_packets_per_block);
-		return (1);
+		return 1;
 	}
-
 
     packet_header_length = packet_header_init(packet_transmit_buffer);
 	fifo_init(fifo, param_fifo_count, param_data_packets_per_block);
 	fifo_open(fifo, param_fifo_count);
 	fifo_create_select_set(fifo, param_fifo_count, &fifo_set, &max_fifo_fd);
 
-
-	//initialize forward error correction
+	// Initialize forward error correction
 	fec_init();
 
-
-	// open the interface in pcap
+	// Open the interface in pcap
 	szErrbuf[0] = '\0';
 	ppcap = pcap_open_live(argv[optind], 800, 1, 20, szErrbuf);
 	if (ppcap == NULL) {
 		fprintf(stderr, "Unable to open interface %s in pcap: %s\n",
 		    argv[optind], szErrbuf);
-		return (1);
+		return 1;
 	}
 
-
 	pcap_setnonblock(ppcap, 0, szErrbuf);
+	start_time = time(NULL);
 
-
-
-
- start_time = time(NULL);
- while (!fBrokenSocket) {
- 		fd_set rdfs;
+	while (!fBrokenSocket) {
 		int ret;
-
+ 		fd_set rdfs;
 
 		rdfs = fifo_set;
 
-		//wait for new data on the fifos
+		// Wait for new data on the fifos
 		ret = select(max_fifo_fd + 1, &rdfs, NULL, NULL, NULL);
 
 		if(ret < 0) {
 			perror("select");
-			return (1);
+			return 1;
 		}
 
-		//cycle through all fifos and look for new data
-		for(i=0; i<param_fifo_count && ret; ++i) {
-			if(!FD_ISSET(fifo[i].fd, &rdfs)) {
+		// Cycle through all fifos and look for new data
+		for(int i=0; i<param_fifo_count && ret; ++i) {
+			if(!FD_ISSET(fifo[i].fd, &rdfs))
 				continue;
-			}
 
 			ret--;
 
 			packet_buffer_t *pb = fifo[i].pbl + fifo[i].curr_pb;
 			
-            //if the buffer is fresh we add a payload header
+            // If the buffer is fresh we add a payload header
 			if(pb->len == 0) {
-                pb->len += sizeof(payload_header_t); //make space for a length field (will be filled later)
+                pb->len += sizeof(payload_header_t); // Make space for a length field (will be filled later)
 			}
 
-			//read the data
+			// Read the data
 			int inl = read(fifo[i].fd, pb->data + pb->len, param_packet_length - pb->len);
 			if(inl < 0 || inl > param_packet_length-pb->len){
 				perror("reading stdin");
@@ -437,7 +384,7 @@ main(int argc, char *argv[])
 			}
 
 			if(inl == 0) {
-				//EOF
+				// EOF
 				fprintf(stderr, "Warning: Lost connection to fifo %d. Please make sure that a data source is connected\n", i);
 				usleep(1e5);
 				continue;
@@ -445,33 +392,26 @@ main(int argc, char *argv[])
 
 			pb->len += inl;
 			
-			//check if this packet is finished
+			// Check if this packet is finished
 			if(pb->len >= param_min_packet_length) {
                 payload_header_t *ph = (payload_header_t*)pb->data;
-                ph->data_length = pb->len - sizeof(payload_header_t); //write the length into the packet. this is needed since with fec we cannot use the wifi packet lentgh anymore. We could also set the user payload to a fixed size but this would introduce additional latency since tx would need to wait until that amount of data has been received
+                ph->data_length = pb->len - sizeof(payload_header_t); // Write the length into the packet. this is needed since with fec we cannot use the wifi packet lentgh anymore. We could also set the user payload to a fixed size but this would introduce additional latency since tx would need to wait until that amount of data has been received
                 pcnt++;
 
-				//check if this block is finished
+				// Check if this block is finished
 				if(fifo[i].curr_pb == param_data_packets_per_block-1) {
                     pb_transmit_block(fifo[i].pbl, ppcap, &(fifo[i].seq_nr), i+param_port, param_packet_length, packet_transmit_buffer, packet_header_length, param_data_packets_per_block, param_fec_packets_per_block, param_transmission_count);
 					fifo[i].curr_pb = 0;
 				}
-				else {
+				else
 					fifo[i].curr_pb++;
-				}
-
 			}
 		}
 
-
-		if(pcnt % 128 == 0) {
+		if(pcnt % 128 == 0)
 			printf("%d data packets sent (interface rate: %.3f)\n", pcnt, 1.0 * pcnt / param_data_packets_per_block * (param_data_packets_per_block + param_fec_packets_per_block) / (time(NULL) - start_time));
-		}
-
 	}
 
-
 	printf("Broken socket\n");
-
 	return (0);
 }
